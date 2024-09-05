2d98ca1e6c8a675f9587ebca9fa06dac51fd9a7f
marvi2011 <marvi661@hotmail.com>
2024-09-05 13:20:06 -0300

    agregamos el componente Informacion con texto sobre React

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/Informacion";
 
 function App() {
-  
-
   return (
-    <main className='container my-4'>
-    <h1 className='text-center'>Primer Proyecto React</h1>
-    <p>Lorem ipsum dolor, sit amet <br></br> consectetur adipisicing elit. Ipsa dolore illo minima nisi consequatur<br/> similique nesciunt, voluptates ratione! Recusandae reprehenderit laudantium laborum, atque facilis, consectetur quidem reiciendis repellat voluptas dicta necessitatibus sapiente cupiditate assumenda ullam accusantium temporibus fuga ea eligendi ipsam aspernatur! Rerum accusantium asperiores iusto molestiae quos iste reiciendis.
-    </p>
+    <main className="container my-4">
+      <h1 className="text-center">Primer Proyecto React</h1>
+      <p>
+        Lorem ipsum dolor, sit amet <br></br> consectetur adipisicing elit. Ipsa
+        dolore illo minima nisi consequatur
+        <br /> similique nesciunt, voluptates ratione! Recusandae reprehenderit
+        laudantium laborum, atque facilis, consectetur quidem reiciendis
+        repellat voluptas dicta necessitatibus sapiente cupiditate assumenda
+        ullam accusantium temporibus fuga ea eligendi ipsam aspernatur! Rerum
+        accusantium asperiores iusto molestiae quos iste reiciendis.
+      </p>
+      {/*aqui invoco a los componentes para poder usarlos, se hace como etiqueta html */}
+      <Informacion></Informacion>
     </main>
-   
-  )
+  );
 }
 
-export default App
+export default App;
