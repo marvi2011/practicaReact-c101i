714072ddb6feb02f748e5e718804c98304142e6f
Victoria Gramajo <marvi661@hotmail.com>
2024-09-04 21:58:48 -0300

    comenzamos el maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   
 
   return (
-   <h1>Hola mundo</h1>
+    <main className='container my-4'>
+    <h1 className='text-center'>Primer Proyecto React</h1>
+    <p>Lorem ipsum dolor, sit amet <br></br> consectetur adipisicing elit. Ipsa dolore illo minima nisi consequatur<br/> similique nesciunt, voluptates ratione! Recusandae reprehenderit laudantium laborum, atque facilis, consectetur quidem reiciendis repellat voluptas dicta necessitatibus sapiente cupiditate assumenda ullam accusantium temporibus fuga ea eligendi ipsam aspernatur! Rerum accusantium asperiores iusto molestiae quos iste reiciendis.
+    </p>
+    </main>
+   
   )
 }
 
